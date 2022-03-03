79bc2f2298bce91f96286465dde12c69b2004d26
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-03-04 00:48:14 +0200

    Set up sass, styled splash screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 export default function App() {
+  const [started, setStarted] = React.useState(false);
+  
+  function startQuiz() {
+    setStarted(true);
+  }
+
+  const splashScreen = (
+    <div className="splash">
+      <h1>Quizzical</h1>
+      <p>Questions from the Open Trivia Database</p>
+      <button onClick={startQuiz}>Start quiz</button>
+    </div>
+  );
+  
   return (
     <div className="container">
-      <h1>App component</h1>
+      {!started && splashScreen}
     </div>
   );
 };
