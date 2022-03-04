3c594395297d36ddaf3ce5fba4c2393ff9ca3b9d
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-03-04 15:56:59 +0200

    hitting "start quiz" loads quiz questions from API to state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 export default function App() {
+  const amount = 5;
+  const category = 9;
+  const difficulty = 'medium';
+  const apiUrl = 'https://opentdb.com/api.php?'
   const [started, setStarted] = React.useState(false);
-  
+  const [questions, setQuestions] = React.useState([]);
+
+  console.log(questions);
+
   function startQuiz() {
     setStarted(true);
+  }
+
+  function getQuestions(num, cat, diff) {
+    fetch(
+      `${apiUrl}amount=${num}&category=${cat}`
+      + `&difficulty=${diff}&type=multiple`
+    ).then(
+      res => res.json()
+    ).then(
+      data => setQuestions(data)
+    );
   }
 
   const splashScreen = (
@@ -14,10 +32,23 @@ export default function App() {
       <button onClick={startQuiz}>Start quiz</button>
     </div>
   );
+
+  React.useEffect(() => {
+    console.log('effect ran')
+    if (started) {
+      getQuestions(amount, category, difficulty);
+    }
+  }, [started]);
   
   return (
     <div className="container">
-      {!started && splashScreen}
+      {
+        started 
+        ? 
+        <h1>placeholder</h1>
+        : 
+        splashScreen
+      }
     </div>
   );
 };
